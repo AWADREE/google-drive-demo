087024e76f7e90e4368929ff68fe90bb6c592a67
AWADREE <62517784+AWADREE@users.noreply.github.com>
2021-06-04 22:53:21 +0200

    added the ability to folders
    added the ability to folders by creating a folder in firestore that has created at, name, user id

---
+++ src/components/google-drive/AddFolderButton.js
@@ -0,0 +1,67 @@
+import { Button, Form, Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
+import { database } from "../../firebase";
+import { useAuth } from "../../contexts/AuthContext";
+
+export default function AddFolderButton(currentFolder) {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const { currentUser } = useAuth();
+
+  function openModel() {
+    setOpen(true);
+  }
+
+  function closeModal() {
+    setOpen(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (currentFolder == null) return;
+
+    //create a folder in the database
+    database.folders.add({
+      name: name,
+      parentId: currentFolder.id,
+      userId: currentUser.uid,
+      //   path,
+      createdAt: database.getCurrentTimestamp(),
+    });
+    setName("");
+    closeModal();
+  }
+  return (
+    <>
+      <Button onClick={openModel} variant="outline-success" size="sm">
+        <FontAwesomeIcon icon={faFolderPlus} />
+      </Button>
+      <Modal show={open} onHide={closeModal}>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Folder Name</Form.Label>
+              <Form.Control
+                type="text"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Close
+            </Button>
+            <Button variant="success" type="submit">
+              Add Folder
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  );
+}
